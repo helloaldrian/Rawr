49c4425f9268d6fdaf05ff9354758d2610ab6180
Jiyuu <7382086+helloaldrian@users.noreply.github.com>
2018-10-14 04:29:44 +0800

    added
    download.sh for db

---
+++ data/download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/attributes.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/books.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/cards.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/collections.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/cubes.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/equipment.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/equipment_sets.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/gems.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/items.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/jobs.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/maps.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/monsters.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/recipes.csv
+wget https://raw.githubusercontent.com/rjgtav/tos-database/master/web/src/assets/data/skills.csv

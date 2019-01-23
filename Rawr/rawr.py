0ddbc62dc16f0d1761397beffc3220ca8160202b
TheIgni <theigni@gmail.com>
2019-01-23 13:04:02 +0100

    Update rawr.py

--- Rawr/rawr.py
+++ Rawr/rawr.py
@@ -379,14 +379,14 @@ async def time():
 async def lv(ctx):
     pages = [
 "**Read :** FAQ for newbie and returning player!! \n**Command :** !rawr faq \n\n_- Navigate using the reaction emotes._\n_- Navigation buttons/reactions can only be used by individual who input the **!rawr lv** command._\n_- Guide start from page 2._\n_- Press_ \N{INFORMATION SOURCE} _if you're confused._",
-"***Lvl 1 ~ 50:***\n\nKill **Hanamings** and other mobs at **East Siauliai Woods** and **West Siauliai Woods** for abit until you have enough silver to get Blessing and Sacrament buffs from pardoners in Klaipeda (usually about 3000 silver is enough). Get the buffs then run all the way to **Tenet Garden** and kill the mobs above the goddess statue there, they are plentiful and have fast respawn times. You can also choose to go inside **Tenet Church B1** and kill stuff there if you wish.",
-"***Lvl 50 ~ 114:***\n\nGo to **Feretory Hills** and kill the **Hallowventers** there along with other mobs. The current state of game makes it so that it is difficult to find matches in lower level dungeon queues unless you shout or are in a guild. therefore grinding here is more effective than waiting in town for a queue pop. You can also continue to grind here if you have already ran out of dungeon runs if you were able to get a match. Once you get closer to lvl 100ish, you may also choose to go grind at **Sicarius 1f**. From **level 100**, you can start to level up using the Challenge mode feature, it is best to go with a party around your level when attempting Challenge mode.",
-"***Lvl 114 ~ 202:***\n\nMake your way to **Demon Prison District 2** and kill the mobs there, they are plentiful and respawn quickly. You will be able to grind here for quite a while due to the sheer number of mobs and the fast respawn rate. If you find that there are too many people at Demon Prison District 2, around level 130ish you can make your way to **2nd Demon Prison** and grind the mobs there instead. **(The maps 2nd Demon Prison and Demon Prison District 2 are different maps, do not confuse them as the same map)**, An alternate path is to try the Challenge Mode feature around maps of your level.",
-"***Lvl 202 ~ 230:***\n\nGrind around **Cranto Coast** and do the Challenge mode there. If you still need abit more exp to reach 230 then consider doing some quests too. **Neighport Church East Building** is a great place to grind too once your daily challenge modes are done.",
-"***Lvl 230 ~ 270:***\n\nGo to **Kalejimas Visiting Room** and grind the various mobs there for exp. Remember to keep up with your daily dungeon runs and Challenge mode entries. If you still need exp in this stage, then consider doing a few quests around that area or 100 percent explore the maps for exp cards which you can redeem at the **Wings of Viboria NPC at Klaipeda**.",
-"***Lvl 270 ~ 315:***\n\nKeep up with Daily dungeons around your level and make your way to **Timerys Temple**. This will be your new home for grinding and Challenge modes until lvl 315. Make sure to use up to date equipment for your level because the mobs will hurt. Timerys Temple also happens to be a great map for gem farming and silver farming.",
-"***Lvl 315 ~ 350:***\n\nYou can now start to level at **Sausys Room 9**, this is a great place to grind and do your daily challenge modes. Remember to keep up to date with your equipment and work towards better equips. If you are around lvl 340ish and you are getting bored of Sausys Room 9, you can go to **Narvas Temple** for a change of scenery and do the Challenge mode or grind there.", 
-"***Lvl 350 ~ 390:***\n\nHead over to **Spell Tome Town** and do the Challenge modes there, it is a great place for this due to the narrow areas and high level monsters. Supplement your Challenge Mode grinds by questing around areas of **Astral Tower, Barynwell Waters, Spell Tome Town and Starry Town**. Monsters will be abit tough so it is reccomended to attempt the Challenge mode grinds and such with a party. It is also a great area to farm for high level loot."
+"***Lvl 1 ~ 50:***\n\nStart off by completing all the main(yellow/gold) quests from **East Siauliai Woods** all the way to **West Siauliai Woods.** You can then complete the main quest chains from **Miner's Village > Crystal Mines > Strautas Gorge > Gele Plateau > Nefritas Cliff > Tenet Garden.** Once you have enough silver to get Blessing and Sacrament buffs from pardoners in Klaipeda town(usually about 1600 silver is enough), you will want to get the buffs whenever you want to level. Make sure you refresh your buffs if they run out by purchasing them again in town. Once you arrive at **Tenet Garden **you can kill the mobs above the goddess statue there, they are plentiful and have fast respawn times. You will also need to go inside **Tenet Church B1** and complete the entire main questline there until **Tenet Church 2f** in order to acquire the **Seal of Space** quest item.",
+"***Lvl 50 ~ 114:***\n\nOnce you are lvl 50, you will be able to attempt the **lvl 50 dungeon in Klaipeda** town, be sure to use your megaphones brought from the tp store using free daily tp to shout for people to queue up for it or join a guild so that it is easier to find parties. Go to **Feretory Hills** and kill the **Hallowventers** there along with other mobs. Be sure to complete the quests in **Feretory Hills, Mochia Forest** and **Sutatis Trade Route** for some easy exp. You can continue to return to **Feretory Hills** and grind the mobs if you've already ran out of dungeon runs for the day and have completed the previous quests. Once you get closer to lvl 90ish, you may also start to grind at **Sicarius 1f.**",
+"***Lvl 114 ~ 202:***\n\nMake your way to **Aqueduct Bridge Area** and start the main quest chain there. It will bring you into the **Demon Prison** maps in which you can follow the entire questchain until **Demon Prison District 5.** Once you have done so, you may choose to return to **Demon Prison District 2** and kill the mobs there, they are plentiful and respawn quickly. You will be able to grind here for quite a while due to the sheer number of mobs and the fast respawn rate. you will also want to try the Challenge Mode feature(lvl 100+) around maps of your level. An alternate path is to go to **Dina Bee Farm** and kill the bees in **Akasya Field** and **Rododun Apiary** areas when you are around lvl 140+(you can also do the quests in this map too!)",
+"***Lvl 202 ~ 230:***\n\nMake your way to **Alemeth Forest** and complete the quests there. Continue on and complete the quests in **Barha Forest, Nahash Forest, Cranto Coast** and **Igti Coast.** **Cranto Coast** is also an excellent map to do Challenge modes in, be sure to attempt challenge modes in a party from now on(you can get party members from shouting a party link via megaphone). If you still need abit more exp to reach 230 then consider visiting **Neighport Church East Building,** it is a great place to grind once your daily challenge modes and previous quests are done.",
+"***Lvl 230 ~ 270:***\n\nGo to **Kalejimas Visiting Room** and start to complete the main quests in that area. You can actually follow the entire quest route from **Kalejimas Visiting Room > Storage > Solitary Cells > Workshop > Investigation Room** killing all the monsters you encounter on the way. Remember to keep up with your daily dungeon runs of appropriate level and Challenge mode entries. If you still require additional exp to reach lvl 270 you can consider returning to **Workshop** and grinding the monsters there, it features a relatively small map along with a decent amount of monster spawns.",
+"***Lvl 270 ~ 315:***\n\nKeep up with Daily dungeons around your level and make your way to **Timerys Temple.** This will be your new home for grinding and Challenge modes until lvl 315. Make sure to use up to date equipment for your level because the mobs will start to hurt alot. **Timerys Temple** also happens to be a great map for gem farming and silver farming. An alternative path to this would be to go to **Inner Wall District 8** and complete the main quests there. You can continue to follow the questchain around **City Wall 8, Jeromel Commemorative Park** and **Jonael Commemoratie Orb**. If you feel like you require some addition exp, you can consider returning to **Jeromel Commemorative Park** to grind the plentiful mobs there.",
+"***Lvl 315 ~ 357:***\n\nYou can now start to level at **Sausys Room 9,** this is a great place to grind and do your daily challenge modes for silver, loot and exp. Remember to keep up to date with your equipment and work towards better equips. Complete all the quests from **Sausys Room 9** all the way to **Valandis Room 91.** When you are around lvl 340ish and you are getting bored of grinding at **Sausys Room 9,** you can go to **Narvas Temple** for a change of scenery and do the Challenge modes, quests and grind there.",
+"***Lvl 357 ~ 390:***\n\nYou will want to complete most of the higher level quests in this bracket range for their great exp amounts. Start from **Lanko 26 Waters** and complete all the quests from there until **Barynwell 87 Waters,** then head into **Astral Tower 1f. Astral Tower 1F** happens to be a great place for challenge modes with its narrow corridors forcing mobs to spawn in close proximity running towards you. Complete all the quests within **Astral Tower 1f** until **Astral tower 21f** then make your way to **Starry Town.** You will want to follow the quest chain here and complete all the quests in **Starry Town, Feline Post Town** and finally **Spell Tome Town. Spell Tome Town** is also an excellent place to do challenge modes due to specific narrow areas and high level monsters. Remember to keep up with your dungeon runs of appropriate levels. If you still require additional exp, you may choose to grind around any of the higher level maps in this bracket range or spam challenge mode runs with reset vouchers from events and from purchasing them off other players in the market(the profit from a single cm run is usually greater than the cost of purchasing a reset voucher)"
     ]
 
     for index, page in enumerate(pages):
@@ -412,7 +412,6 @@ async def pnt():
     patch = """
 **[Ktest/Ktos - Patch Notes Translation]**
 _(no longer maintained)_
-
 **Greyhiem's**  https://pastebin.com/u/Greyhiem
 **Gwenyth's**  https://pastebin.com/u/sunhwapark
 """
@@ -429,14 +428,11 @@ async def get(ctx, *name):
     
     getout = """
 :warning: **__502 BAD GATEWAY__** :warning:
-
 **ReadMe**
 ```css
 [ Hi there, this is Rawr! ]
-
 Since #tos.neet.tv are no longer maintained and/or accessible by Rawr, I can no longer provide a quick search & dispaly both #items and #skills information for you.
 This feature will be disabled for the time being.
-
 [ I am sorry for the inconvenience! ]
 ```
 **Alternative:**
@@ -449,11 +445,9 @@ This feature will be disabled for the time being.
     # get keyword #
     Deprecated = """
     name = '+'.join(name)
-
     r = urllib.request.urlopen('https://tos.neet.tv/items?name=' + name + '&f=1').read()
     soup = BeautifulSoup(r, 'html.parser')
     result_table = soup.find('table', {"class": 'results-table'}).find('tbody').find_all('tr')
-
     item_names = []
     item_types = []
     item_links = []
@@ -461,30 +455,21 @@ This feature will be disabled for the time being.
     res_len = len(result_table)
     start = 0
     end = 7
-
     for no, row in enumerate(result_table[start:end], start=0):
         columns = row.find_all('td')
         item_links.append(columns[1].find('a').get('href'))
         item_names.append(columns[2].string)
         item_types.append(columns[3].string)
-
         result_search += str(no + 1) + '. ' + columns[2].get_text() + ' - [' + columns[3].get_text() + ']' + '\n'
-
-
     # send search result - multiple choice #
     msg = await bot.say(content=ctx.message.author.mention + "\n**Please choose one by giving its number**,\n_type `next` or `>` to display more result._" + "```" + str(result_search) + "```" + "\n")
-
-
     # waiting for response from user #
     while True:
         choice = await bot.wait_for_message(timeout=30.0, author=ctx.message.author)
-
         if choice is None:
             await bot.say('_**too slow...**_   **(╯°□°）╯︵ ┻━┻**')
             break
-
         elif choice.content == 'next' or choice.content == '>':
-
                     start += 7
                     end += 7
                     result_search = ""
@@ -493,23 +478,16 @@ This feature will be disabled for the time being.
                         item_links.append(columns[1].find('a').get('href'))
                         item_names.append(columns[2].string)
                         item_types.append(columns[3].string)
-
                         result_search += str(no + 1) + '. ' + columns[2].get_text() + ' - [' + columns[3].get_text() + ']' + '\n'
-
                     await bot.delete_message(msg)
-
                     msg = await bot.say(content=ctx.message.author.mention + "\n**Please choose one by giving its number**,\n_type `next` or `>` to display more result._" + "```" + str(result_search) + "```" + "\n")
-
-
     # send search result - embed #
         elif choice.content.isdigit() and int(choice.content) >= 1 and int(choice.content) <= len(result_search):
             choice_number = int(choice.content)
             embed = get_item('https://tos.neet.tv' + item_links[choice_number - 1])
-
             # await bot.delete_message(choice)
             await bot.delete_message(msg)
             # await bot.delete_message(ctx.message)
-
             await bot.say(content=ctx.message.author.mention + "\n**This is your search result!**\n_Click the item name to see more info on your browser._", embed=embed)
             break
     """
@@ -524,14 +502,11 @@ async def skill(ctx, *job):
     
     getout = """
 :warning: **__502 BAD GATEWAY__** :warning:
-
 **ReadMe**
 ```css
 [ Hi there, this is Rawr! ]
-
 Since #tos.neet.tv are no longer maintained and/or accessible by Rawr, I can no longer provide a quick search & dispaly both #items and #skills information for you.
 This feature will be disabled for the time being.
-
 [ I am sorry for the inconvenience! ]
 ```
 **Alternative:**
@@ -545,85 +520,60 @@ This feature will be disabled for the time being.
     noskill = """
     # get keyword #
     __location__ = os.path.realpath( os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
     with open(os.path.join(__location__, 'classes2.json')) as f:
     # with open('classes2.json') as f:
         content = f.read()
-
     tos_classes2 = json.loads(content)
-
     for tos_class in tos_classes2:
         tos_class['regex'] = re.compile(tos_class['regex'], re.IGNORECASE)
-
     response = "\nNot Found\n"
-
     keyword = ' '.join(job)
-
     for tos_class in tos_classes2:
         if tos_class['regex'].match(keyword):
             code, name = tos_class['code'], tos_class['name']
             # print('Founded?: ' + name + " :" + code)
             break
-
     jobs = code
-
     r = urllib.request.urlopen('https://tos.neet.tv/skills?cls=' + jobs + '&f=1').read()
     soup = BeautifulSoup(r, 'html.parser')
     result_table = soup.find('table', {"class": 'results-table'}).find('tbody').find_all('tr')
-
     skill_names = []
     skill_types = []
     skill_links = []
     result_search = ''
     skill_res = ''
     res_len = len(result_table)
-
     for no, row in enumerate(result_table):
         columns = row.find_all('td')
         skill_links.append(columns[1].find('a').get('href'))
         skill_names.append(columns[2].string)
         skill_types.append(columns[3].string)
-
         result_search += str(no + 1) + '. ' + columns[2].get_text() + ' - [' + columns[3].get_text() + ']' + ' ------ ' + columns[1].find('a').get('href') + '\n'
         skill_res += str(no + 1) + '. ' + columns[2].string + '\n'
-
     # send search result - multiple choice #
     msg = await bot.say(content=ctx.message.author.mention + "\n**Please choose one by giving its number:**" + "```" + (skill_res) + "```" + "\n")
-
     # waiting for response from user #
     while True:
         choice = await bot.wait_for_message(timeout=30.0, author=ctx.message.author)
-
         if choice is None:
             await bot.say('_**too slow...**_   **(╯°□°）╯︵ ┻━┻**')
             break
-
     # send search result - embed #
         elif choice.content.isdigit() and int(choice.content) >= 1 and int(choice.content) <= len(result_search):
             choice = int(choice.content)
             items = skill_info('https://tos.neet.tv' + skill_links[choice - 1])
-
             embed = discord.Embed(colour=discord.Colour(0xD2EE8A), description=items['description'], timestamp=datetime.datetime.now())
-
             # embed.set_image(url="https://tos.neet.tv/images/equip/icon_item_shirts_acolyte_silver.png")
             embed.set_thumbnail(url=items['thumbnail'])
-
             embed.set_author(name=items['title'], url='https://tos.neet.tv' + skill_links[choice - 1], icon_url="http://bestonlinegamesreview.com/wp-content/uploads/2016/04/p1_2006411_5eae6fd9.png")
-
             embed.set_footer(text="tos.neet.tv", icon_url="https://tos.neet.tv/images/hairacc/hairacc_80_fez.png")
-
             sklinfo = '```' + '\n'.join(["{:<15}: {}".format(*item) for item in items['adin'].items()]) + '```'
-
             embed.add_field(name="Skill Info", value=sklinfo, inline=True)
-
             if len(items['attribs']) > 0:
                 sklatrb = '```' +'\n\n'.join(["{}\n{}".format(*item.values()) for item in items['attribs']]) + '```'
                 embed.add_field(name="Attributes", value=sklatrb, inline=False)
-
             await bot.delete_message(msg)
-
             await bot.say(content=ctx.message.author.mention + "\n**This is your search result!**\n_Click the skill name to see more info on your browser._", embed=embed)
-
             break
             """
 
